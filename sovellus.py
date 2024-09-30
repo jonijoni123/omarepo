a8c76d1e05b6c34a29e27a37f8684bec24a0cd92
juhay-git <juha.yli-hemminki@seamk.fi>
2024-09-30 20:28:30 +0300

    Lisätty tulostusfunktio

--- sovellus.py
+++ sovellus.py
@@ -1,3 +1,13 @@
 #sovellus -jonip
 
 print("hello World")
+
+def hello(n):
+    if n == 1:
+        print("!!!")
+        return 1
+    else:
+        print("hello", end=" ")
+        return hello(n - 1)
+
+hello(4)
